dd86b914c69a2b50af9dd9ab5c5da77bfe731040
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-05-17 02:28:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,21 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+########### 修改默认 IP ###########
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+########### 设置密码为空（可选） ###########
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+########### 更改大雕源码（可选）###########
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
+
+########### 更改默认主题（可选）###########
+# 删除自定义源默认的 argon 主题
+# rm -rf package/lean/luci-theme-argon
+# 拉取 argon 原作者的源码
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 替换默认主题为 luci-theme-argon
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+# make menuconfig时记得勾选LuCI ---> Applications ---> luci-app-argon-config
